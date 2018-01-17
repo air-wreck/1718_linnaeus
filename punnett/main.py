e44216ca22f06a6766dea791aa6c5aada73f94ab
kayan19 <kayan@ctemc.org>
2018-01-17 13:17:19 -0500

    main
    main stuff

---
+++ punnett/main.py
@@ -0,0 +1,12 @@
+import os.path
+import punnett4x4
+import punnett8x8
+import punnett16x16
+
+print ('\n\nPunnett Square Creator\n')
+print ('This program can support the follow square sizes:\n')
+print ('\tEnter \'1\' for 2x2')
+print ('\tEnter \'2\' for 4x4')
+print ('\tEnter \'3\' for 8x8')
+print ('\tEnter \'4\' for 16x16')
+print ('\nPlease enter a square size:')
