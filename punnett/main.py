4e016a8c36b63466dadac87aa70931cc3c66991e
kayan19 <kayan@ctemc.org>
2018-01-17 13:25:45 -0500

    moar main
    a

--- punnett/main.py
+++ punnett/main.py
@@ -1,4 +1,5 @@
 import os.path
+import punnett2x2
 import punnett4x4
 import punnett8x8
 import punnett16x16
@@ -9,4 +10,17 @@ print ('\tEnter \'1\' for 2x2')
 print ('\tEnter \'2\' for 4x4')
 print ('\tEnter \'3\' for 8x8')
 print ('\tEnter \'4\' for 16x16')
-print ('\nPlease enter a square size:')
+
+while True:
+    command = raw_input('Please enter a square size: ')
+    if command == '1':
+        break
+    elif command == '2':
+        break
+    elif command == '3':
+        break
+    elif command == '4':
+        break
+    else:
+        print ('Command not recognized. Please try again.')
+print "blegh"
