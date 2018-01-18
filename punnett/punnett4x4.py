87ce15af4f71e8967a3c9dd7ca1c3ba0676c4da4
Karena Yan <kayan@ctemc.org>
2018-01-17 19:34:48 -0500

    user interface
    framework of main done, just need to add 2x2 square and help document and fancy stuff

--- punnett/punnett4x4.py
+++ punnett/punnett4x4.py
@@ -1,59 +1,52 @@
 import matplotlib.pyplot as plt
+import re
 
 def makeSquare4():
-    p1 = raw_input("Please enter the alleles of the father.")
-    p2 = raw_input("Please enter the alleles of the mother.")
-    global inherit
-    inherit = inheritlist(raw_input("Please enter autd for autosomal, inc for incomplete dominance, cod for codominance, and xl for x-linked."))
-    go = test(p1, p2)
-    if go == 1:
-        print "You inputted more than four alleles for each parent. Please try again."
-    elif go == 2:
-        print "You used more than two letters to represent alleles. Please try again."
-    elif go ==3:
-        print "You used fewer than two letters to represent alleles. Please try again."
-    else:
-        gametes1 = []
-        gametes2 = []
-        for i in range(2):
-            for j in range(2,4):
-                gametes1.append(p1[i]+p1[j])
-                gametes2.append(p2[i]+p2[j])
-        data = [[],[],[],[]]
-        count = 0
-        for g1 in gametes2:
-            data[count].append(g1)
-            for g2 in gametes1:
-                data[count].append(formatS(g1[0]+g2[0])+formatS(g1[1]+g2[1]))
-            count+=1
-        phenprobs = prob(p1, p2)
-        print phenprobs
-        colors = setColors(data)
-        #text = analyzeData(data)
-        table = plt.table(
-            cellText=data,
-            cellColours=colors,
-            cellLoc='center',
-            colWidths=[0.2,0.2,0.2,0.2,0.2],
-            rowLabels=['','','',''],
-            colLabels=['']+gametes1,
-            colColours= ['0.45','0.45','0.45','0.45','0.45'],
-            colLoc='center',
-            loc='center',bbox=None)
-        table.scale(1, 4)
-        plt.title(inheritance)
-        plt.axis('off')
-        plt.show()
-        plt.savefig('image.png',dpi=750)
+    while True:
+        p1 = raw_input("Please enter the alleles of the father: ").strip()
+        p2 = raw_input("Please enter the alleles of the mother: ").strip()
+        go = test(p1, p2)
+        if go == '':
+            break
+        else:
+            print go
+    
+    gametes1 = []
+    gametes2 = []
+    for i in range(2):
+        for j in range(2,4):
+            gametes1.append(p1[i]+p1[j])
+            gametes2.append(p2[i]+p2[j])
+            
+    data = [[],[],[],[]]
+    count = 0
+    for g1 in gametes2:
+        data[count].append(g1)
+        for g2 in gametes1:
+            data[count].append(formatS(g1[0]+g2[0])+formatS(g1[1]+g2[1]))
+        count+=1
         
-def inheritlist(x):
-        return {
-            'autd': "Autosomal Dominance",
-            'inc': "Incomplete Dominance",
-            'cod': "Codominance",
-            'xl': 'X-Linked',
-            }.get(x, "defaulted to Autosomal Dominant")
-
+    phenprobs = prob(p1, p2)
+    colors = setColors(data)
+    #text = analyzeData(data)
+    
+    table = plt.table(
+        cellText=data,
+        cellColours=colors,
+        cellLoc='center',
+        colWidths=[0.2]*5,
+        rowLabels=['']*4,
+        colLabels=['']+gametes1,
+        colColours= ['0.45']*5,
+        colLoc='center',
+        loc='center',bbox=None)
+    table.scale(1, 4)
+    plt.axis('off')
+    plt.show()
+    #plt.savefig('image.png',dpi=750)
+    
+    return phenprobs
+        
 def prob(g1,g2):
     g1a = g1[0:2]
     g1b = g1[2:4]
@@ -62,10 +55,11 @@ def prob(g1,g2):
     probsa = probOne(g1a,g2a)
     probsb = probOne(g1b,g2b)
     phenprobs = {}
-    phenprobs['22'] = probsa['2']*probsb['2']
-    phenprobs['20'] = probsa['2']*probsb['0']
-    phenprobs['02'] = probsa['0']*probsb['2']
-    phenprobs['00'] = probsa['0']*probsb['0']
+    plist = ('0','2')
+    for a in plist:
+        for b in plist:
+            tag = a+b
+            phenprobs[tag] = probsa[a]*probsb[b]
     return phenprobs
 
 def probOne(g1, g2):
@@ -103,47 +97,44 @@ def setColors(data):
     return colors
 
 def analyzeData(data):
-    text = [['','','',''],['','','',''],['','','',''],['','','','']]
+    text = [['']*4]*4
     for i in range(0,4):
         for j in range(1,5):
             if data[i][j][:2].isupper():
-                text[i][j-1]+="Trait 1: Homozygous dominant. Dominant phenotype."
+                text[i][j-1]+="Trait 1: Homozygous dominant."
             elif data[i][j][:2].islower():
-                text[i][j-1]+="Trait 1: Homozygous recessive. Recessive phenotype."
+                text[i][j-1]+="Trait 1: Homozygous recessive."
             else:
-                text[i][j-1]+="Trait 1: Heterozygous. Dominant phenotype."
+                text[i][j-1]+="Trait 1: Heterozygous."
             if data[i][j][2:4].isupper():
-                text[i][j-1]+="Trait 2: Homozygous dominant. Dominant phenotype."
+                text[i][j-1]+="\nTrait 2: Homozygous dominant."
             elif data[i][j][2:4].islower():
-                text[i][j-1]+="Trait 2: Homozygous recessive. Recessive phenotype."
+                text[i][j-1]+="\nTrait 2: Homozygous recessive."
             else:
-                text[i][j-1]+="Trait 2: Heterozygous. Dominant phenotype."
+                text[i][j-1]+="\nTrait 2: Heterozygous."
     return text
 
 def test(p1, p2):
-    notEnough = False
-    tooMuch = False
+    p1 = p1.upper()
+    p2 = p2.upper()
+    if re.sub('[^A-Za-x]','',p1) != p1 or re.sub('[^A-Za-x]','',p2) != p2:
+        return '\nPlease input letters for alleles. Try again.'
+    if len(p1)!=4 or len(p2) !=4:
+        return '\nPlease input exactly four alleles for each parent. Try again.'
     letters = []
     for p in (p1, p2):
         letters2 = []
         for letter in p:
-            if letter.upper() not in letters2:
-                letters2.append(letter.upper())
-            if letter.upper() not in letters:
-                letters.append(letter.upper())
-        if len(letters2) < 2:
-            notEnough = True
-        if len(letters2) > 2:
-            tooMuch = True
-    if len(letters) < 2:
-        notEnough = True
-    if len(letters) > 2:
-        tooMuch = True
-    if len(p1) != 4 or len(p2) !=4:
-        return 1
-    elif tooMuch:
-        return 2
-    elif notEnough:
-        return 3
-    else:
-        return 4
+            if letter not in letters2:
+                letters2.append(letter)
+            if letter not in letters:
+                letters.append(letter)
+        if len(letters2) != 2:
+            return '\nPlease use two different letters in each parent genotype.Try again.'
+    if len(letters) != 2:
+        return '\nPlease use the same two letters for both parent genotypes. Try again.'
+    if len(p1.strip(p1[0])) !=2 or len(p2.strip(p2[0])) !=2:
+        return '\nPlease provide two alleles for each gene. Try again.'
+    if p1[0].upper() != p1[1].upper() or p2[0].upper() != p2[1].upper():
+        return '\nPlease group the alleles by gene. Try again.'
+    return ''
