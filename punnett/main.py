fd55f160f9ef714e4b3c0b06d658b9d4dfa69b2f
Karena Yan <kayan@ctemc.org>
2018-01-23 20:13:51 -0500

    main fix
    i chinesed a thing with the probablities

--- punnett/main.py
+++ punnett/main.py
@@ -86,6 +86,7 @@ while make: #program loops so user can continue to generate punett squares
             xl = xlink
             cod = codom
             probTable(probs)
+            xlink, cod = False, False
             break
         elif command == '2':
             probs= punnett4x4.makeSquare4()
