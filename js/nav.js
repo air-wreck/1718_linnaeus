76f099dcc7a27719d7248690c669a80692feb793
Eric <zheng.eric.y@gmail.com>
2018-01-31 17:45:38 -0500

    finish JS punnett squares on website and clean up JS structure

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+/* nav bar responsive JS */
+
+window.onclick = function(event) {
+  menu = document.getElementById("dropdown");
+  if (event.target.matches("#dropbtn") || event.target.matches(".menu")) {
+    if (menu.style.display == "none" || menu.style.display == "") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  } else {
+    menu.style.display = "none";
+  }
+};
